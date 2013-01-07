ac03a26765193ec50d3036cb323f93736ae697c5
long <long@long-LD.(none)>
2013-01-08 02:50:32 +0800

    new file.

--- src/test.py
+++ src/test.py
@@ -29,8 +29,8 @@ from vtk.color import exponential_blue
 
 class TrayIcon(gtk.Window):
     def __init__(self):
-        #gtk.Window.__init__(self, gtk.WINDOW_POPUP)
-        gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
+        gtk.Window.__init__(self, gtk.WINDOW_POPUP)
+        #gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
         # init values.
         self.trayicon_x = 10.5
         self.trayicon_y = 10.5
@@ -43,28 +43,89 @@ class TrayIcon(gtk.Window):
         self.draw = gtk.EventBox()
         self.add(self.draw)
         self.draw.connect("expose-event", self.tray_icon_expose_event)
+        #self.draw.connect("
+        self.connect("size-allocate", self.tray_icon_size_allocate)
         self.show_all()
+       
         self.move(500, 500)
         self.draw.add(gtk.Button("测试"))
         
+    def tray_icon_size_allocate(self, widget, rect):
+        import math
+        x, y, w, h = rect.x, rect.y, rect.width, rect.height
+        bitmap = gtk.gdk.Pixmap(None, w, h, 1)        
+        cr = bitmap.cairo_create()
+
+        cr.set_source_rgb(0, 0, 0)
+        cr.set_operator(cairo.OPERATOR_CLEAR)
+        cr.paint()
+
+        cr.set_source_rgb(0, 0, 0)
+        cr.set_operator(cairo.OPERATOR_OVER)
+        #
+        radius = self.radius 
+        x = self.trayicon_x
+        y = self.trayicon_y
+        arrow_width = self.arrow_width
+        arrow_height = self.arrow_height 
+        w = w - 20
+        h = h - 20
+        offs = 30
+        
+        #
+        offs = (w/2)
+        #offs = 50
+        if (offs + 50) > (w + 20):
+            offs = (w + 20) - 15 - arrow_width
+        if (offs < 17):
+            offs = 17
+        # draw.
+        cr.arc (x + radius,
+                y + arrow_height + radius,
+                radius,
+                math.pi,
+                math.pi * 1.5)
+        cr.line_to(offs, y + arrow_height)
+        cr.rel_line_to(arrow_width / 2.0, -arrow_height)
+        cr.rel_line_to(arrow_width / 2.0, arrow_height)
+        cr.arc (x + w - radius,
+                y + arrow_height + radius,
+                radius,
+                math.pi * 1.5,
+                math.pi * 2.0)
+        cr.arc(x + w - radius,
+               y + h - radius,
+               radius,
+               0,
+               math.pi * 0.5)
+        cr.arc(x + radius,
+               y + h - radius,
+               radius,
+               math.pi * 0.5,
+               math.pi)
+        
+        cr.close_path()
+        cr.fill()
+        widget.shape_combine_mask(bitmap, 0, 0)
+    
     def tray_icon_expose_event(self, widget, event):
         cr = widget.window.cairo_create()
         rect = widget.allocation
         x, y, w, h = rect
         # create surface.
         surface, surface_context = new_surface(w, h)
-        #
+        #.  
         cairo_popover(widget, 
                       surface_context, 
                       self.trayicon_x, self.trayicon_y, 
                       w, h,
                       self.radius, 
                       self.arrow_width, self.arrow_height)
-        # shadow.
         surface_context.set_source_rgba(1, 1, 1, 0.9)
         surface_context.fill_preserve()
-        exponential_blue(surface, surface_context, 
-                         6, w, h)
+        # shadow.
+        #exponential_blue(surface, surface_context, 
+        #                 6, w, h)
         surface_context.clip()
         # background.
         #widget.get_style_context()#.render_background(surface_context, 0, 0, w, h)
@@ -77,10 +138,10 @@ class TrayIcon(gtk.Window):
                       self.radius,
                       self.arrow_width, self.arrow_height)
         surface_context.set_operator(cairo.OPERATOR_SOURCE)
-        surface_context.set_line_width(1)
-        surface_context.set_source_rgba(1, 1, 1, 0.9)
+        surface_context.set_line_width(4)
+        surface_context.set_source_rgba(0, 0, 0, 0.9)
         surface_context.stroke()
-        #
+        # clear surface.
         cr.set_operator(cairo.OPERATOR_SOURCE)
         cr.set_source_rgba(1, 1, 1, 1)
         cr.paint()
