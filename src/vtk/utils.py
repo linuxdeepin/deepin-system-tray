b4845aea1ebc357e7ca6270f8928cad7a5fe8cb6
long <long@long-LD.(none)>
2013-01-07 20:42:52 +0800

    add file.

--- src/vtk/utils.py
+++ src/vtk/utils.py
@@ -47,3 +47,45 @@ def cairo_rounded_rectangle(cr, x, y, width, height, radius):
             math.pi * 1.5)
     cr.close_path()
 
+def cairo_popover (widget, 
+                   cr, w, h, 
+                   arrow_width=20, arrow_height=10):
+    w = w - 20
+    h = h - 20
+    offs = 30
+    p_x = widget.window.get_origin()[0]
+    alloc = widget.get_allocation()
+    #
+    #offs = (p_x + alloc.x) - w_x + this.get_
+    offs = -1
+    if (offs + 50) > (w + 20):
+        offs = (w + 20) - 15 - arrow_width
+    if (offs < 17):
+        offs = 17
+    # draw.
+    cr.arc (x + radius,
+            y + arrow_height + radius,
+            radius,
+            math.pi,
+            math.pi * 1.5)
+    cr.line_to(offs, y + arrow_height)
+    cr.rel_line_to(arrow_width / 2.0, -arrow_height)
+    cr.rel_line_to(arrow_width / 2.0, arrow_height)
+    cr.arc (x + w - radius,
+            y + arrow_height + radius,
+            radius,
+            math.pi * 1.5,
+            math.pi * 2.0)
+    cr.arc(x + w - radius,
+           y + h - radius,
+           radius,
+           0,
+           math.pi * 0.5)
+    cr.arc(x + w - radius,
+           y + h - radius,
+           radius,
+           math.pi * 0.5,
+           math.pi)
+    
+    cr.close_path()
+
